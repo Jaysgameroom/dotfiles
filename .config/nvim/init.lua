c587f67c84ff3dca4bcf8752c6ca25878d4a28ee
Josiah Ricketts <josiahricketts8@gmail.com>
2026-02-02 03:36:22 -0600

    idk

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,6 +5,11 @@ vim.o.swapfile = false
 vim.o.signcolumn = "yes"
 vim.o.shiftwidth= 4
 vim.o.wrap = false
+vim.o.wrapscan = true
+vim.o.ignorecase = true
+vim.o.scrolloff = 2
+
+
 
 vim.g.mapleader = ' '
 
@@ -19,6 +24,9 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 vim.keymap.set('n', '<leader>gs', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>`', ':e ~/.config/nvim/init.lua<CR>')
 
+vim.keymap.set('n', '<leader>1', ':colorscheme gruvbox<CR>')
+vim.keymap.set('n', '<leader>2', ':colorscheme kanagawa<CR>')
+
 vim.pack.add({
 	{src = "https://github.com/neovim/nvim-lspconfig.git"},
 	{src = "https://github.com/windwp/nvim-autopairs.git"},
@@ -29,19 +37,32 @@ vim.pack.add({
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter.git"},
 	{src = "https://github.com/ibhagwan/fzf-lua.git"},
 	{src = "https://github.com/brenoprata10/nvim-highlight-colors.git"},
+	{src = "https://github.com/saghen/blink.cmp.git"},
+	{src = "https://github.com/rebelot/kanagawa.nvim.git"},
+	{src = "https://github.com/lewis6991/gitsigns.nvim.git"},
 })
 
 require("nvim-autopairs").setup()
 require("mason").setup()
 require("oil").setup()
-require("mini.icons").setup()
 require("fzf-lua").setup()
 require("nvim-highlight-colors").setup()
 require("nvim-treesitter").setup()
+require("gitsigns").setup()
+require("blink.cmp").setup({
+	keymap = {preset = "super-tab"}
+})
 
-vim.lsp.enable({"clangd", "pylsp", "jdtls"})
+require("mini.icons").setup({
+	directory = {
+		downloads = {glyph = '󰉍', hl = 'MiniIconsOrange'},
+		music = {glyph = '󱍙', hl = 'MiniIconsOrange'}
+	}	
+})
 
-vim.cmd("colorscheme gruvbox")
+
+vim.lsp.enable({"glsl_analyzer", "clangd", "intelephense", "ts_ls", "html", "cssls", "gdscript"})
+
 vim.cmd("set background=dark")
 
 vim.cmd[[set completeopt+=menuone,noselect,popup]]
