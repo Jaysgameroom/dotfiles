7a50cedacbe200aa1bd159c670431655d11c7b1f
Josiah Ricketts <josiahricketts8@gmail.com>
2026-02-10 21:29:10 -0600

    removed colorschemes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,6 @@ vim.o.wrap = false
 vim.o.wrapscan = true
 vim.o.ignorecase = true
 vim.o.scrolloff = 2
-
 vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>w', ':write<CR>')
@@ -24,16 +23,11 @@ vim.keymap.set('n', '<leader>gs', ':update<CR> :source<CR>')
 vim.keymap.set('n', '<leader>`', ':e ~/.config/nvim/init.lua<CR>')
 vim.keymap.set('n', '<leader>rain', ':CellularAutomaton make_it_rain<CR>')
 vim.keymap.set('n', '<leader>scram', ':CellularAutomaton scramble<CR>')
+vim.keymap.set('n', '<leader>~', ':e ~/code<CR>')
 
 vim.keymap.set('n', '<leader>1', ':colorscheme gruvbox<CR>')
-vim.keymap.set('n', '<leader>2', ':colorscheme kanagawa<CR>')
-vim.keymap.set('n', '<leader>3', ':colorscheme catppuccin<CR>')
-vim.keymap.set('n', '<leader>4', ':colorscheme onedark<CR>')
-vim.keymap.set('n', '<leader>5', ':colorscheme tokyonight<CR>')
-vim.keymap.set('n', '<leader>6', ':colorscheme everforest<CR>')
-vim.keymap.set('n', '<leader>7', ':colorscheme nord<CR>')
-vim.keymap.set('n', '<leader>8', ':colorscheme monokai<CR>')
 vim.keymap.set('n', '<leader>9', ':colorscheme vscode<CR>')
+
 
 
 
@@ -44,7 +38,8 @@ vim.pack.add({
 	{src = "https://github.com/stevearc/oil.nvim.git"},
 		--fuzzy find
 	{src = "https://github.com/ibhagwan/fzf-lua.git"},
-		--treesitter stuff
+		--text objects
+	{src = "https://github.com/nvim-mini/mini.ai.git"},
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects.git"},
 
 	--Editing
@@ -52,12 +47,13 @@ vim.pack.add({
 	{src = "https://github.com/saghen/blink.cmp.git"},
 		--autpairs
 	{src = "https://github.com/windwp/nvim-autopairs.git"},
-
+	{src = "https://github.com/windwp/nvim-ts-autotag.git"},
 
 	--Parsing
 		--lsp
 	{src = "https://github.com/mason-org/mason.nvim.git"},
 	{src = "https://github.com/neovim/nvim-lspconfig.git"},
+	{src = "https://github.com/mason-org/mason-lspconfig.nvim.git"},
 		--treesitter
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter.git"},
 
@@ -68,27 +64,19 @@ vim.pack.add({
 	{src = "https://github.com/nvim-mini/mini.icons.git"},
 		--color highlighting
 	{src = "https://github.com/brenoprata10/nvim-highlight-colors.git"},
-		--colorschemes
-	{src = "https://github.com/rebelot/kanagawa.nvim.git"},
-	{src = "https://github.com/joshdick/onedark.vim.git"},
-	{src = "https://github.com/catppuccin/nvim.git"},
-	{src = "https://github.com/folke/tokyonight.nvim.git"},
-	{src = "https://github.com/sainnhe/everforest.git"},
-	{src = "https://github.com/shaunsingh/nord.nvim.git"},
-	{src = "https://github.com/tanvirtin/monokai.nvim.git"},
-	{src = "https://github.com/Mofiqul/vscode.nvim.git"},
+
 	{src = "https://github.com/ellisonleao/gruvbox.nvim.git"},
+	{src = "https://github.com/Mofiqul/vscode.nvim.git"},
 		--git-signs
 	{src = "https://github.com/lewis6991/gitsigns.nvim.git"},
 	{src = "https://github.com/nvim-mini/mini.surround.git"},
-
+		--markdown
 	{src = "https://github.com/MeanderingProgrammer/render-markdown.nvim.git"},
+		--animation
+	{src = "https://github.com/karb94/neoscroll.nvim.git"},
 
 	--Fun
 	{src = "https://github.com/Eandrju/cellular-automaton.nvim.git"},
-	{src = "https://github.com/rafamadriz/friendly-snippets.git"},
-	{src = "https://github.com/windwp/nvim-ts-autotag.git"},
-	{src = "https://github.com/karb94/neoscroll.nvim.git"},
 })
 
 
@@ -102,27 +90,39 @@ require("gitsigns").setup()
 require("mini.surround").setup()
 require("fidget").setup()
 require("nvim-ts-autotag").setup()
-require("neoscroll").setup({duration_multiplier= 0.3, easing = 'sine'})
 
-
-ftypes = {'cpp', 'javascript', 'html', 'css', 'php', 'lua', "java"}
-
-require("nvim-treesitter").setup()
-require("nvim-treesitter").install(ftypes)
-
-vim.o.syntax = off
-
-
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = ftypes,
-  callback = function() 
-	  	vim.treesitter.start() 
-		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-  end,
+require("neoscroll").setup({
+	duration_multiplier = 0.3
 })
 
 
+local ai = require('mini.ai').gen_spec
+
+require('mini.ai').setup({
+	custom_textobjects = {
+		f = ai.treesitter({ a = '@function.outer', i = '@function.inner' }),
+		o = ai.treesitter({
+			a = { '@conditional.outer', '@loop.outer' },
+			i = { '@conditional.inner', '@loop.inner' },
+		}),
+		c = ai.treesitter({ a = "@class.outer", i = "@class.inner"}),
+		u = ai.function_call()
+
+	}
+})
+
+require("mason-lspconfig").setup({
+	ensure_installed = {"lua_ls", "clangd", "ts_ls", "hyprls"}
+})
+
+
+local ftypes = {'cpp', 'javascript', 'html', 'css', 'php', 'lua', "java"}
+
+
+vim.o.syntax = off
+
+require("nvim-treesitter").setup()
+require("nvim-treesitter").install(ftypes)
 
 require("blink.cmp").setup({
 	keymap = {preset = "super-tab"},
@@ -133,12 +133,17 @@ require("mini.icons").setup({
 	directory = {
 		downloads = {glyph = '󰉍', hl = 'MiniIconsOrange'},
 		music = {glyph = '󱍙', hl = 'MiniIconsOrange'}
-	}	
+	}
 })
 
-require("gruvbox").setup({
-	contrast = "",
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = ftypes,
+	callback = function()
+		vim.treesitter.start()
+		vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+	end
 })
+
 
 vim.diagnostic.config({
 	virtual_text = {
@@ -153,35 +158,18 @@ vim.diagnostic.config({
 	}
 })
 
-vim.lsp.enable({"jdtls", "rust_analyzer", "clangd", "intelephense", "ts_ls", "html", "cssls"})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+		vim.highlight.on_yank({timeout=10})
+  end,
+})
+
+
+
 
 vim.cmd("set background=dark")
 vim.cmd("colorscheme gruvbox")
 
 vim.cmd[[set completeopt+=menuone,noselect,popup]]
-
-vim.keymap.set({ "x", "o" }, "af", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
-end)
-
-vim.keymap.set({ "x", "o" }, "if", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
-end)
-
-vim.keymap.set({ "x", "o" }, "ac", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
-end)
-
-vim.keymap.set({ "x", "o" }, "ic", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
-end)
-
--- You can also use captures from other query groups like `locals.scm`
-vim.keymap.set({ "x", "o" }, "as", function()
-  require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")
-end) 
-
-vim.keymap.set("n", "<leader>a", function()
-  require("nvim-treesitter-textobjects.swap").swap_next "@function.outer"
-end)
 
