21611d2aa5fce0a317984f05a35c357ba689fcfb
Josiah Ricketts <josiahricketts8@gmail.com>
2026-02-22 06:43:03 -0600

    figures

--- .config/nvim/snippets/tex.lua
+++ .config/nvim/snippets/tex.lua
@@ -20,8 +20,23 @@ return {}, {
 	s("dm", fmt("\\[\n \t{} \n\\]", {i(1)} )),
 	s("im", fmt("${}$", {i(1)} )),
 	s("sn", fmt("\\sin", {})),
+	s("sn", fmt("\\sin", {})),
+	s("sn", fmt("\\sin", {})),
+
 	s("cs", fmt("\\cos", {})),
-	s({regTrig=true,trig="(%S+)/(%S+)"},  f(function(_, snip) return "\\frac{" .. snip.captures[1] .. "}{" .. snip.captures[2] .."}" end)),
-	s({regTrig=true,trig="(%S+)|(%S+)"},  f(function(_, snip) return "\\int_{" .. snip.captures[1] .. "}^{" .. snip.captures[2] .."}" end))
+	s({regTrig=true,trig="(%S+)/(%S+)"},  f(function(_, snip) return "\\frac{" .. snip.captures[1] .. "}{" .. snip.captures[2] end)),
+	s({regTrig=true,trig="(%S+)|(%S+)"},  f(function(_, snip) return "\\int_{" .. snip.captures[1] .. "}^{" .. snip.captures[2] end)),
+
+	s("cir", c(1, {
+		fmt("\\draw ({}, {}) circle ({}pt);", {i(1), i(2), i(3)}),
+		fmt("\\draw [{}] ({}, {}) circle ({}pt);", {i(1, "fill"), i(2), i(3), i(4),})
+
+	})),
+
+	s("line", c(1, {
+		fmt("\\draw ({},{}) -- ({},{});", {i(1), i(2), i(3), i(4)}),
+		-- fmt("\\draw [{}] ({},{}) -- ({},{});", {i(1, "->"), i(2), i(3), i(4), i(5)}),
+	})),
+	s("force", fmt("\\draw [{}] ({},{}) -- ({},{}) node [{}] {{${}$}};", {i(1, "->"), i(2), i(3), i(4), i(5), c(6, {t"above", t"below", t"left", t"right"}), i(7)}))
 
 }
