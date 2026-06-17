775309daf201c4e95c30873cd9a26a3e84908fce
Josiah Ricketts <josiahricketts8@gmail.com>
2026-06-17 05:27:25 -0500

    stuf

---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,12 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.tabstop = 4
+vim.o.swapfile = false
+vim.o.signcolumn = "yes"
+vim.o.shiftwidth= 4
+vim.o.wrap = false
+vim.o.wrapscan = true
+vim.o.ignorecase = true
+vim.o.scrolloff = 2
+vim.g.mapleader = ' '
+
