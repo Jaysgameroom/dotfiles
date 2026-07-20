3feeed134a914db78e37d7adb71b0bebe783ed01
Josiah Ricketts <josiahricketts8@gmail.com>
2026-07-19 23:44:13 -0500

    refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,3 +5,22 @@ require("lsp")
 require("treesitter")
 require("cmp")
 require("oil1")
+
+vim.pack.add({
+	"https://github.com/windwp/nvim-autopairs",
+})
+
+require("nvim-autopairs").setup()
+vim.diagnostic.config({
+	virtual_text = {
+		prefix = "■",
+		source = "if_many",
+		spacing = 4,
+	},
+	float = {
+		focusable = false,
+		border = "rounded",
+		source = "always"
+	}
+})
+
